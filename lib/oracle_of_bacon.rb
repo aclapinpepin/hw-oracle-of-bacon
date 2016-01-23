0fb2d02b616d32548970c15810f7410a3923965b
aclapinpepin <aclapinpepin@gmail.com>
2016-01-23 02:27:22 +0100

    Finish all tests but one

--- lib/oracle_of_bacon.rb
+++ lib/oracle_of_bacon.rb
@@ -10,9 +10,11 @@ class OracleOfBacon
   class NetworkError < RuntimeError ; end
   class InvalidKeyError < RuntimeError ; end
 
+  API_KEY = '38b99ce9ec87'
+
   attr_accessor :from, :to
   attr_reader :api_key, :response, :uri
-  
+
   include ActiveModel::Validations
   validates_presence_of :from
   validates_presence_of :to
@@ -20,11 +22,13 @@ class OracleOfBacon
   validate :from_does_not_equal_to
 
   def from_does_not_equal_to
-    # YOUR CODE HERE
+    errors.add(:from, "can't use same name twice") if from == to
   end
 
-  def initialize(api_key='')
-    # your code here
+  def initialize(api_key=API_KEY)
+    @api_key = api_key
+    @from = 'Kevin Bacon'
+    @to = 'Kevin Bacon'
   end
 
   def find_connections
@@ -37,15 +41,18 @@ class OracleOfBacon
       # convert all of these into a generic OracleOfBacon::NetworkError,
       #  but keep the original error message
       # your code here
+      byebug
+      errors.add(OracleOfBacon::NetworkError, e.message)
     end
     # your code here: create the OracleOfBacon::Response object
+    @response = Response.new(xml)
   end
 
   def make_uri_from_arguments
-    # your code here: set the @uri attribute to properly-escaped URI
-    #   constructed from the @from, @to, @api_key arguments
+    escaped_values = [api_key, from, to].map { |v| CGI.escape(v) }
+    @uri = "http://oracleofbacon.org/cgi-bin/xml?p=#{escaped_values[0]}&a=#{escaped_values[1]}&b=#{escaped_values[2]}"
   end
-      
+
   class Response
     attr_reader :type, :data
     # create a Response object from a string of XML markup.
@@ -59,11 +66,28 @@ class OracleOfBacon
     def parse_response
       if ! @doc.xpath('/error').empty?
         parse_error_response
-      # your code here: 'elsif' clauses to handle other responses
-      # for responses not matching the 3 basic types, the Response
-      # object should have type 'unknown' and data 'unknown response'         
+      elsif ! @doc.xpath('/link').empty?
+        parse_graph_response
+      elsif ! @doc.xpath('/spellcheck').empty?
+        parse_spellcheck_response
+      else
+        @data = 'unknown response'
+        @type = :unknown
       end
     end
+
+    def parse_graph_response
+      actors = @doc.xpath('//actor').collect { |a| a.text }
+      movies = @doc.xpath('//movie').collect { |m| m.text }
+      @data = actors.zip(movies).flatten.compact
+      @type = :graph
+    end
+
+    def parse_spellcheck_response
+      @data = @doc.xpath('//match').collect { |n| n.text }
+      @type = :spellcheck
+    end
+
     def parse_error_response
       @type = :error
       @data = 'Unauthorized access'
